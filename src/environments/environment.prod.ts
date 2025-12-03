6e0d9e3b25d1957e6a2814647a38ddbe0464624e
Jose Ivan Gomez Vazquez <jose.gomezvaz@alumno.buap.mx>
2025-12-03 01:06:02 -0600

    Env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  // Reemplaza esto con TU URL real de Render
+  url_api: 'https://proyectofinalaplicacioneswebback.onrender.com' 
 };
